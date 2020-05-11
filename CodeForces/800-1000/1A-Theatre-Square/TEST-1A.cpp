96c19e8e7db42139e263d1cb366213c2cf7e3976
Erik Töger <erik.toger@gmail.com>
2020-05-11 08:59:33 +0200

    Add folders with difficulty level

--- CodeForces/800-1000/1A-Theatre-Square/TEST-1A.cpp
+++ CodeForces/800-1000/1A-Theatre-Square/TEST-1A.cpp
@@ -1,5 +1,5 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
-#include "../../catch.hpp"
+#include "../../../catch.hpp"
 #include "1A-Theatre-Square.h"
 using namespace  std;
 
