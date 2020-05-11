e515e833679c9fd470350c00d01240f88689ad64
Erik Töger <erik.toger@gmail.com>
2020-05-11 20:07:47 +0200

    550A-Two-Strings

--- CodeForces/1500/550A-Two-Strings/550A-Two-Strings.cpp
+++ CodeForces/1500/550A-Two-Strings/550A-Two-Strings.cpp
@@ -1,3 +1,34 @@
+#include <iostream>
+#include <vector>
 
+using namespace std;
 
+void twostrings() {
+    string str;
+    cin >> str;
+    char A = 'A';
+    char B = 'B';
+    vector<int> indexesAB;
+    vector<int> indexesBA;
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == B) {
+            if (i > 0 && str[i - 1] == A) {
+                indexesAB.emplace_back(i);
+            }
+            if (i < str.length() - 1 && str[i + 1] == A) {
+                indexesBA.emplace_back(i);
+            }
+        }
+    }
+    if(indexesAB.empty() || indexesBA.empty()){
+        cout << "NO";
+        return;
+    }
+    if(indexesAB.size() == 1 && indexesBA.size() == 1 && indexesAB[0] == indexesBA[0]){
+        cout<< "NO";
+        return;
+    }
 
+    cout << "YES";
+
+}
---
+++ CodeForces/1500/550A-Two-Strings/TEST-550A.cpp
@@ -0,0 +1,55 @@
+#define CATCH_CONFIG_MAIN
+
+#include "550A-Two-Strings.h"
+#include "../../../catch.hpp"
+
+using namespace std;
+
+
+TEST_CASE("Example 1", "Template") {
+    stringbuf test_input("ABA", ios_base::in);
+    stringbuf test_output(ios_base::out);
+    streambuf *const cin_buf = cin.rdbuf(&test_input);
+    streambuf *const cout_buf = cout.rdbuf(&test_output);
+
+    twostrings();
+
+    cout.rdbuf(cout_buf);
+    cin.rdbuf(cin_buf);
+    string test_output_text = test_output.str();
+
+    REQUIRE(test_output_text == "NO");
+}
+
+TEST_CASE("Example 2", "Template") {
+    stringbuf test_input("BACFAB", ios_base::in);
+    stringbuf test_output(ios_base::out);
+    streambuf *const cin_buf = cin.rdbuf(&test_input);
+    streambuf *const cout_buf = cout.rdbuf(&test_output);
+
+    twostrings();
+
+    cout.rdbuf(cout_buf);
+    cin.rdbuf(cin_buf);
+    string test_output_text = test_output.str();
+
+    REQUIRE(test_output_text == "YES");
+}
+
+TEST_CASE("Example 3", "Template") {
+    stringbuf test_input("AXBYBXA", ios_base::in);
+    stringbuf test_output(ios_base::out);
+    streambuf *const cin_buf = cin.rdbuf(&test_input);
+    streambuf *const cout_buf = cout.rdbuf(&test_output);
+
+    twostrings();
+
+    cout.rdbuf(cout_buf);
+    cin.rdbuf(cin_buf);
+    string test_output_text = test_output.str();
+
+    REQUIRE(test_output_text == "NO");
+}
+
+
+
