b1e59c680abbcbc44b86dc0c54e0a98ca8a766ae
Erik Töger <erik.toger@gmail.com>
2020-06-06 12:54:50 +0200

    Update test

--- Other/TEST-quick.cpp
+++ Other/TEST-quick.cpp
@@ -8,7 +8,8 @@ using namespace std;
 
 TEST_CASE("Example 1", "Template") {
 string word = "afdsflse";
-cout << word << endl;
+
 quickSort(word,0,word.size()-1);
-cout << word <<endl;
+
+REQUIRE(word == "adefflss");
 }
