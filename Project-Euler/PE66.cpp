2e27b8a0af1a3a88742f90b120b1a859d27f8df5
Erik Töger <erik.toger@gmail.com>
2020-12-06 20:49:19 +0100

    Refactor

--- Project-Euler/PE66.cpp
+++ Project-Euler/PE66.cpp
@@ -1,59 +1,57 @@
 #include "../Other/BigInteger/BigInteger.h"
 #include <cmath>
+
 using namespace std;
-int main(){
+
+int main() {
 
 
-    int result = 0;
-    BigInteger pmax = BigInteger("0");
+    int answer = 0;
+    BigInteger largestX = "0"s;
 
-    for(int D =2; D <= 1000; D++){
+    for (int D = 2; D <= 1000; D++) {
         int sqr = sqrt(D);
-        if( sqr*sqr == D){
+        if (sqr * sqr == D) {
             continue;
         }
-        BigInteger bigD = BigInteger(to_string(D));
-        BigInteger limit = BigInteger(to_string(sqr));
+        BigInteger bigD = to_string(D);
+        BigInteger limit = to_string(sqr);
 
-        BigInteger m = BigInteger("0");
-        BigInteger d = BigInteger("1");
-        BigInteger a = BigInteger(limit.getNumber());
+        BigInteger m = "0"s;
+        BigInteger d = "1"s;
+        BigInteger a = limit.getNumber();
 
-        BigInteger numm1 = BigInteger("1");;
-        BigInteger num = BigInteger(a.getNumber());
+        BigInteger numm1 = "1"s;
+        BigInteger num = a.getNumber();
 
-        BigInteger denm1 = BigInteger("0");
-        BigInteger den = BigInteger("1");
+        BigInteger denm1 = "0"s;
+        BigInteger den = "1"s;
 
-        while( true ){
-            m = (d * a) - m;
-            d = (bigD - (m * m) ) / d;
+        while (true) {
+            m = d * a - m;
+            d = (bigD - m * m) / d;
             a = (limit + m) / d;
 
-            BigInteger numm2 = BigInteger(numm1.getNumber());
-            numm1 = BigInteger(num.getNumber());
-            BigInteger denm2 = BigInteger(denm1.getNumber());
-            denm1 = BigInteger(den.getNumber());
+            BigInteger numm2 = numm1.getNumber();
+            numm1 = num.getNumber();
+            BigInteger denm2 = denm1.getNumber();
+            denm1 = den.getNumber();
 
-            num = (a*numm1) + numm2;
-            den = (a * denm1) + denm2;
+            num = a * numm1 + numm2;
+            den = a * denm1 + denm2;
 
-            BigInteger breaking = (num*num) - (bigD*den*den);
-            if(breaking.getNumber() =="1" && breaking.getIsPositive()){
+            BigInteger breaking = num * num - bigD * den * den;
+            if (breaking.getNumber() == "1" && breaking.getIsPositive()) {
                 break;
             }
         }
 
-        if (pmax < num) {
-            pmax = BigInteger(num.getNumber());
-            result = D;
+        if (largestX < num) {
+            largestX = num.getNumber();
+            answer = D;
         }
 
-
-
-
-
     }
-    cout << pmax.getNumber()<<endl;
-    cout << result <<endl;
+    cout << largestX.getNumber() << endl;
+    cout << answer << endl;
 }
