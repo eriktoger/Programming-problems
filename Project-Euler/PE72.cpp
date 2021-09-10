f8d47198a014532689a6c92c153b87dbb24282b4
Erik Töger <erik.toger@gmail.com>
2021-09-10 12:10:48 +0200

    Add PE 72

--- Project-Euler/PE72.cpp
+++ Project-Euler/PE72.cpp
@@ -1,4 +1,25 @@
+#include <vector>
+#include <numeric>
+#include <iostream>
 
-void func(std::istream &is, std::ostream &os) {
+using namespace ::std;
 
+int main() {
+
+    int limit = 1000000;
+
+    // using eulers totient function
+    vector<long int> sieve(limit + 1);
+    std::iota(sieve.begin(), sieve.end(), 0);
+    long result = 0;
+    for (int i = 2; i <= limit; i++) {
+        if (sieve[i] == i) {
+            for (int j = i; j <= limit; j += i) {
+                double di = i;
+                sieve[j] = sieve[j]*(1 - 1/di) ;
+            }
+        }
+        result += sieve[i];
+    }
+    cout << result << endl;
 }
