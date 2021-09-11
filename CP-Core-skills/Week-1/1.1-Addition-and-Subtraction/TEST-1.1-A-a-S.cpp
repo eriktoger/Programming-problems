96c9d51e64d6b846e0a553dc07b0c7b5263c890d
Erik Töger <erik.toger@gmail.com>
2021-09-11 20:56:53 +0200

    gitignore fix

--- CP-Core-skills/Week-1/1.1-Addition-and-Subtraction/TEST-1.1-A-a-S.cpp
+++ CP-Core-skills/Week-1/1.1-Addition-and-Subtraction/TEST-1.1-A-a-S.cpp
@@ -1,10 +1,11 @@
 #define CATCH_CONFIG_MAIN
+
 #include "../../../catch.hpp"
 #include "1.1-A-a-S.h"
 
 using namespace std;
 
-TEST_CASE( "Input 3", "Template" ) {
+TEST_CASE("Input 3", "Template") {
     stringbuf test_input("2 1 3", ios_base::in);
     stringbuf test_output(ios_base::out);
     streambuf *const cin_buf = cin.rdbuf(&test_input);
@@ -16,10 +17,10 @@ TEST_CASE( "Input 3", "Template" ) {
     cin.rdbuf(cin_buf);
     string test_output_text = test_output.str();
 
-    REQUIRE(test_output_text== "3");
+    REQUIRE(test_output_text == "3");
 }
 
-TEST_CASE( "Input 2", "Template" ) {
+TEST_CASE("Input 2", "Template") {
     stringbuf test_input("2 0 3", ios_base::in);
     stringbuf test_output(ios_base::out);
     streambuf *const cin_buf = cin.rdbuf(&test_input);
@@ -31,5 +32,5 @@ TEST_CASE( "Input 2", "Template" ) {
     cin.rdbuf(cin_buf);
     string test_output_text = test_output.str();
 
-    REQUIRE(test_output_text== "-1");
+    REQUIRE(test_output_text == "-1");
 }
