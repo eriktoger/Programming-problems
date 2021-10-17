538f3b374f655c070cc92627fb9eea69851865d2
Erik Töger <erik.toger@gmail.com>
2021-10-17 12:00:00 +0200

    New problem: 2.3-Minimums

---
+++ CP-Core-skills/Week-2/2.3-Minimums/2.3-Minimums.cpp
@@ -0,0 +1,29 @@
+#include "2.3-Minimums.h"
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+void func( std::istream &is , std::ostream &os ){
+    int totalNumbers;
+    vector<int> numbers;
+    cin >> totalNumbers;
+    while (totalNumbers--)
+    {
+        int num;
+        cin >> num;
+        numbers.emplace_back(num);
+       
+    }
+    totalNumbers = numbers.size();
+    long long int answer = 0;
+    for (int i = 0; i < totalNumbers; i++)
+    {
+        int minimum = 9999999;
+        for (int j = i; j < totalNumbers; j++)
+        {
+            minimum = min(minimum, numbers[j]);
+            answer += minimum;
+        }
+    }
+        cout << answer;
+} 
---
+++ CP-Core-skills/Week-2/2.3-Minimums/2.3-Minimums.h
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifndef COMPETITIVE_PROGRAMMING_2_3_MINIMUMS_H
+#define COMPETITIVE_PROGRAMMING_2_3_MINIMUMS_H
+
+void func(std::istream &is = cin, std::ostream &os = cout);
+
+#endif //COMPETITIVE_PROGRAMMING_2_3_MINIMUMS_H
