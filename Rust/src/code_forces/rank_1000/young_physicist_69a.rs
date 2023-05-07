733bf1dab78536a43ab4d5c79d935fbb04922071
Erik Töger <erik.toger@gmail.com>
2023-05-07 19:27:12 +0200

    Add tests and test setup

--- Rust/src/code_forces/rank_1000/young_physicist_69a.rs
+++ Rust/src/code_forces/rank_1000/young_physicist_69a.rs
@@ -1,15 +1,14 @@
-use std::io::{self, Error};
+use std::io::{BufRead, Error, Write};
 
-pub fn get_solution() -> Result<(), Error> {
+pub fn get_solution<R: BufRead, W: Write>(reader: &mut R, writer: &mut W) -> Result<(), Error> {
     let mut lines = String::new();
-
-    io::stdin().read_line(&mut lines)?;
+    reader.read_line(&mut lines)?;
 
     let lines = lines.trim().parse::<usize>().unwrap();
     let mut sums = [0, 0, 0];
     for _ in 0..lines {
         let mut line = String::new();
-        io::stdin().read_line(&mut line)?;
+        reader.read_line(&mut line)?;
         let numbers = line.split(" ");
         for (i, number) in numbers.enumerate() {
             sums[i] += number.trim().parse::<i32>().unwrap()
@@ -17,9 +16,33 @@ pub fn get_solution() -> Result<(), Error> {
     }
 
     match sums.iter().find(|number| **number != 0) {
-        Some(_) => println!("NO"),
-        None => println!("YES"),
-    }
+        Some(_) => writeln!(writer, "NO"),
+        None => writeln!(writer, "YES"),
+    }?;
 
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::io::BufReader;
+
+    #[test]
+    fn test_get_solution_no() {
+        let input = "3\n4 1 7\n-2 4 -1\n1 -5 -3\n";
+        let expected_output = "NO\n";
+        let mut output = Vec::new();
+        get_solution(&mut BufReader::new(input.as_bytes()), &mut output).unwrap();
+        assert_eq!(output, expected_output.as_bytes());
+    }
+
+    #[test]
+    fn test_get_solution_yes() {
+        let input = "3\n3 -1 7\n-5 2 -4\n2 -1 -3\n";
+        let expected_output = "YES\n";
+        let mut output = Vec::new();
+        get_solution(&mut BufReader::new(input.as_bytes()), &mut output).unwrap();
+        assert_eq!(output, expected_output.as_bytes());
+    }
 }
--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -1,6 +1,10 @@
 mod code_forces;
 use code_forces::rank_1000::young_physicist_69a::get_solution;
+use std::io::{self, Write};
 
 fn main() {
-    let _ = get_solution();
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+    let _ = get_solution(&mut stdin.lock(), &mut stdout);
+    stdout.flush().unwrap();
 }
