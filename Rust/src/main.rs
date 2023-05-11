9b575c09c807ea3b8b421e30918ff5e687cbe1a9
Erik Töger <erik.toger@gmail.com>
2023-05-11 07:48:35 +0200

    Update main

--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -8,12 +8,23 @@ fn main() {
     std::io::Write::flush(&mut stdout).unwrap();
 }
 
-use std::io::{BufRead, Error, Write};
+use std::{
+    io::{BufRead, Error, ErrorKind, Write},
+    str::FromStr,
+};
 
-pub fn get_solution<R: BufRead, W: Write>(reader: &mut R, writer: &mut W) -> Result<(), Error> {
+fn read_line<R: BufRead, T: FromStr>(reader: &mut R) -> Result<T, Error> {
     let mut lines = String::new();
     reader.read_line(&mut lines)?;
-    writeln!(writer, "Answer")?;
+    match lines.trim().parse::<T>() {
+        Ok(val) => Ok(val),
+        Err(_) => Err(Error::new(ErrorKind::Other, "Parsing failed")),
+    }
+}
+
+pub fn get_solution<R: BufRead, W: Write>(reader: &mut R, writer: &mut W) -> Result<(), Error> {
+    let lines: String = read_line(reader)?;
+    writeln!(writer, "{lines}")?;
 
     Ok(())
 }
