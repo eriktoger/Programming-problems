c9d3f06d3e3c4489853c6b191f3898e6ed9ec7a7
Erik Töger <erik.toger@alfalaval.com>
2023-11-14 08:48:28 +0100

    Set up googletest on windows

--- generateOutput.cpp
+++ generateOutput.cpp
@@ -1,67 +1,56 @@
-#include "myPath.h"
-#include <string>
-#include <iostream>
 #include <cstdio>
 #include <fstream>
+#include <iostream>
+#include <string>
+
+#include "myPath.h"
 
 using namespace std;
 
-void printMain(ofstream &outfile)
-{
+void printMain(ofstream &outfile) {
     outfile << "\n";
     outfile << "int main() {\n";
     outfile << "   ios_base::sync_with_stdio(false);\n";
     outfile << "   cin.tie(NULL);\n";
-    outfile << "   func();\n";
+    outfile << "   solution();\n";
     outfile << "   return 0;\n";
     outfile << "}\n";
 }
 
-void readHeaderFile(ofstream &outfile, string path)
-{
+void readHeaderFile(ofstream &outfile, string path) {
     string headerFilename = "main.h";
     ifstream headerfile;
     headerfile.open(path + headerFilename);
 
-    if (headerfile.is_open())
-    {
+    if (headerfile.is_open()) {
         std::string line;
 
-        while (std::getline(headerfile, line))
-        {
-            if (line.find("COMPETITIVE_PROGRAMMING_MAIN_H") == string::npos)
-            {
+        while (std::getline(headerfile, line)) {
+            if (line.find("COMPETITIVE_PROGRAMMING_MAIN_H") == string::npos) {
                 outfile << line << "\n";
             }
         }
     }
 }
 
-void readCppFile(ofstream &outfile, string path)
-{
+void readCppFile(ofstream &outfile, string path) {
     string cppFilename = "main.cpp";
     ifstream cppfile;
     cppfile.open(path + cppFilename);
 
-    if (cppfile.is_open())
-    {
+    if (cppfile.is_open()) {
         std::string line;
 
-        while (std::getline(cppfile, line))
-        {
-            if (line.find("#include \"main.h\"") == string::npos)
-            {
+        while (std::getline(cppfile, line)) {
+            if (line.find("#include \"main.h\"") == string::npos) {
                 outfile << line << "\n";
             }
         }
     }
 }
 
-int main(int argc, char *argv[])
-{
-
-    if (argc != 2)
-    {
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
         cout << "Enter the path as argument\n";
     }
     myPath.append(argv[1]);
---
+++ generateProblemGTest.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+site=$1
+diffculty=$2
+prefix=$3
+name=$4
+folder=$prefix-$name
+
+
+cd "$site/$diffculty"
+mkdir $folder
+cd $folder
+
+touch main.h
+
+echo "#include <iostream>
+
+using namespace std;
+
+#ifndef COMPETITIVE_PROGRAMMING_MAIN_H
+#define COMPETITIVE_PROGRAMMING_MAIN_H
+
+void solution(std::istream &is = cin, std::ostream &os = cout);
+
+#endif //COMPETITIVE_PROGRAMMING_MAIN_H" >> main.h
+
+touch TEST.cpp
+echo "#include <gtest/gtest.h>
+#include <iostream>
+#include <sstream>
+#include "\""main.h"\""
+
+TEST(Input_Output, inputoutput) {
+    std::ostringstream test_output;
+    stringstream fake_input("\""1"\"");
+    
+    solution(fake_input, test_output);
+
+    EXPECT_EQ("\""1"\"", test_output.str());
+}
+" >> TEST.cpp
+
+
+touch main.cpp
+
+echo "#include "\""main.h"\""
+
+void solution(std::istream &is, std::ostream &os)
+{
+    int testcases;
+
+    is >> testcases;
+
+    os << testcases;
+}" >> main.cpp
+
+touch output.cpp
+
+touch CMakeLists.txt 
+
+echo "cmake_minimum_required(VERSION 3.22)
+cmake_policy(SET CMP0135 NEW)
+project($prefix)
+include(FetchContent)
+FetchContent_Declare(
+  googletest
+  URL https://github.com/google/googletest/archive/b10fad38c4026a29ea6561ab15fc4818170d1c10.zip
+)
+# For Windows: Prevent overriding the parent project's compiler/linker settings
+set(gtest_force_shared_crt ON CACHE BOOL "\"""\"" FORCE)
+FetchContent_MakeAvailable(googletest)
+
+
+enable_testing()
+
+add_executable(
+  TEST
+  TEST.cpp
+  main.cpp
+)
+target_link_libraries(
+  TEST
+  GTest::gtest_main
+
+)
+
+include(GoogleTest)
+gtest_discover_tests(TEST)" >> CMakeLists.txt
+
+
+cmake . -G "MinGW Makefiles"
