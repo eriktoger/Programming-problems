24d13b909f5704c0c315f3ff12047edb0595b402
Töger <erik.toger@gmail.com>
2024-12-02 07:25:40 +0100

    Aoc 2024 02 part 2

--- AdventOfCode/2024/Day02/solutionTwo.cpp
+++ AdventOfCode/2024/Day02/solutionTwo.cpp
@@ -2,5 +2,71 @@
 
 vector<string> solutionTwo(vector<string> const &input)
 {
-    return input;
+    auto reports = splitLinesToWords(input);
+    int safeCounter = 0;
+    for (auto report : reports)
+    {
+
+        bool isIncreasing = true;
+        for (int i = 0; i < report.size(); i++)
+        {
+            int lastIncreasingLevel = -1;
+            isIncreasing = true;
+            for (int j = 0; j < report.size(); j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+                auto level = report[j];
+                auto currentLevel = stoi(level);
+
+                if ((currentLevel <= lastIncreasingLevel || currentLevel - lastIncreasingLevel > 3) && lastIncreasingLevel != -1)
+                {
+                    isIncreasing = false;
+                    break;
+                }
+                lastIncreasingLevel = currentLevel;
+            }
+
+            if (isIncreasing)
+            {
+                break;
+            }
+        }
+        bool isDecreasing = true;
+        for (int i = 0; i < report.size(); i++)
+        {
+            isDecreasing = true;
+            int lastDecreasingLevel = 99999;
+
+            for (int j = 0; j < report.size(); j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+                auto level = report[j];
+                auto currentLevel = stoi(level);
+                if ((currentLevel >= lastDecreasingLevel || lastDecreasingLevel - currentLevel > 3) && lastDecreasingLevel != 99999)
+                {
+                    isDecreasing = false;
+                    break;
+                }
+                lastDecreasingLevel = currentLevel;
+            }
+
+            if (isDecreasing)
+            {
+                break;
+            }
+        }
+
+        if (isIncreasing || isDecreasing)
+        {
+            safeCounter++;
+        }
+    }
+    vector<string> result{to_string(safeCounter)};
+    return result;
 }
