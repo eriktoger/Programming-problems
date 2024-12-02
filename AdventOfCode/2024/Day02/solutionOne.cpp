f5b20c9121802ebb661d968940ed79fa52b008c9
Töger <erik.toger@gmail.com>
2024-12-02 06:33:50 +0100

    AoC 2024 02 part one

--- AdventOfCode/2024/Day02/solutionOne.cpp
+++ AdventOfCode/2024/Day02/solutionOne.cpp
@@ -2,5 +2,41 @@
 
 vector<string> solutionOne(vector<string> const &input)
 {
-    return input;
+    auto reports = splitLinesToWords(input);
+    int safeCounter = 0;
+    for (auto report : reports)
+    {
+        bool isIncreasing = true;
+        int lastIncreasingLevel = -1;
+        for (auto level : report)
+        {
+            auto currentLevel = stoi(level);
+
+            if ((currentLevel <= lastIncreasingLevel || currentLevel - lastIncreasingLevel > 3) && lastIncreasingLevel != -1)
+            {
+                isIncreasing = false;
+                break;
+            }
+            lastIncreasingLevel = currentLevel;
+        }
+
+        bool isDecreasing = true;
+        int lastDecreasingLevel = 99999;
+        for (auto level : report)
+        {
+            auto currentLevel = stoi(level);
+            if ((currentLevel >= lastDecreasingLevel || lastDecreasingLevel - currentLevel > 3) && lastDecreasingLevel != 99999)
+            {
+                isDecreasing = false;
+                break;
+            }
+            lastDecreasingLevel = currentLevel;
+        }
+        if (isIncreasing || isDecreasing)
+        {
+            safeCounter++;
+        }
+    }
+    vector<string> result{to_string(safeCounter)};
+    return result;
 }
