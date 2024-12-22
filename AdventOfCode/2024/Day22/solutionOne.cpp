c754a180e6c5b7c6cf2a82ffc4e2e0381a11efc6
Töger <erik.toger@gmail.com>
2024-12-22 08:58:13 +0100

    AoC Day 22, part 1

---
+++ AdventOfCode/2024/Day22/solutionOne.cpp
@@ -0,0 +1,47 @@
+#include "solutionOne.h"
+
+unsigned long long mixing(unsigned long long secret, unsigned long long result)
+{
+    return secret ^ result;
+}
+
+unsigned long long pruning(unsigned long long secret)
+{
+    return secret % 16777216;
+}
+
+unsigned long long getNextSecret(unsigned long long secret)
+{
+
+    auto multResult = secret * 64;
+    secret = mixing(secret, multResult);
+    secret = pruning(secret);
+
+    auto divResult = secret / 32;
+    secret = mixing(secret, divResult);
+    secret = pruning(secret);
+
+    auto multResult2 = secret * 2048;
+    secret = mixing(secret, multResult2);
+    secret = pruning(secret);
+    return secret;
+}
+
+vector<string> solutionOne(vector<string> const &input)
+{
+
+    unsigned long long secretsScore = 0;
+
+    for (const auto &line : input)
+    {
+        auto secret = stoull(line);
+        int rounds = 2000;
+        while (rounds--)
+        {
+            secret = getNextSecret(secret);
+        }
+
+        secretsScore += secret;
+    }
+    return {to_string(secretsScore)};
+}
